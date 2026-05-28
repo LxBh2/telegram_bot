2f628d51f0b46542d937728e21795483b9c7419d
Alexey <pevunov@sfedu.ru>
2026-05-28 17:17:33 +0300

    add config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 TOKEN = "8663237394:AAEpkxdrg_vHlw5Xe2oM3ylU5NQg2sgk0Lg"
 
 DB_CONFIG = {
-    "user": "postgres",
-    "password": "postgres",
-    "database": "recruitment_bot",
-    "host": "localhost",
+    "user": "botuser",
+    "password": "1234",
+    "database": "telegram_bot",
+    "host": "127.0.0.1",
     "port": 5432
 }
 
